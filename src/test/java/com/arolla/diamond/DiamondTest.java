d5852cd958230d3f99e57b0370598a93041a13cd
bechraoui amine <bechraoui.amine@gmail.com>
2021-02-08 15:03:08 +0100

    Adding Z case

--- src/test/java/com/arolla/diamond/DiamondTest.java
+++ src/test/java/com/arolla/diamond/DiamondTest.java
@@ -87,4 +87,69 @@ public class DiamondTest {
         assertEquals("   A   \n  B B  \n C   C \nD     D\n C   C \n  B B  \n   A   \n", outContent.toString());
     }
 
+    @Test
+    void should_return_z_diamond_when_z_is_submitted() {
+        // Given
+        Diamond diamond = new Diamond('Z');
+
+        // When
+        diamond.print();
+
+        // Then
+        assertEquals("                         A                         \n" +
+                "                        B B                        \n" +
+                "                       C   C                       \n" +
+                "                      D     D                      \n" +
+                "                     E       E                     \n" +
+                "                    F         F                    \n" +
+                "                   G           G                   \n" +
+                "                  H             H                  \n" +
+                "                 I               I                 \n" +
+                "                J                 J                \n" +
+                "               K                   K               \n" +
+                "              L                     L              \n" +
+                "             M                       M             \n" +
+                "            N                         N            \n" +
+                "           O                           O           \n" +
+                "          P                             P          \n" +
+                "         Q                               Q         \n" +
+                "        R                                 R        \n" +
+                "       S                                   S       \n" +
+                "      T                                     T      \n" +
+                "     U                                       U     \n" +
+                "    V                                         V    \n" +
+                "   W                                           W   \n" +
+                "  X                                             X  \n" +
+                " Y                                               Y \n" +
+                "Z                                                 Z\n" +
+                " Y                                               Y \n" +
+                "  X                                             X  \n" +
+                "   W                                           W   \n" +
+                "    V                                         V    \n" +
+                "     U                                       U     \n" +
+                "      T                                     T      \n" +
+                "       S                                   S       \n" +
+                "        R                                 R        \n" +
+                "         Q                               Q         \n" +
+                "          P                             P          \n" +
+                "           O                           O           \n" +
+                "            N                         N            \n" +
+                "             M                       M             \n" +
+                "              L                     L              \n" +
+                "               K                   K               \n" +
+                "                J                 J                \n" +
+                "                 I               I                 \n" +
+                "                  H             H                  \n" +
+                "                   G           G                   \n" +
+                "                    F         F                    \n" +
+                "                     E       E                     \n" +
+                "                      D     D                      \n" +
+                "                       C   C                       \n" +
+                "                        B B                        \n" +
+                "                         A                         \n",
+                outContent.toString());
+    }
+
+
+
 }
